de5ba9dc9768915389ebffdc1220c37025a45c7b
kwmh457 <kwmh457@gmail.com>
2023-12-28 17:34:55 +0900

    Exam 연습문제

---
+++ Exam/src/test1/test01.java
@@ -0,0 +1,36 @@
+package test1;
+
+
+
+/*
+ * 날짜 :2023/12/28
+ * 이름 : 방정환
+ * 내용 : 자바 자료형 연습문제
+ * 
+ */
+
+public class test01 {
+public static void main(String[] args) {
+	char var1 = 'A';
+	int var2 = 100;
+	long var3 = 2147483648L;
+	float var4 = 3.14f;
+	double var5 = 3.14159;
+	boolean var6 = true;
+	boolean var7 = false;
+	String var8 = "hello";
+    String var9 = null;
+	
+	System.out.println("val1 : " + var1);
+	System.out.println("val2 : " + var2);
+	System.out.println("val3 : " + var3);
+	System.out.println("val4 : " + var4);
+	System.out.println("val5 : " + var5);
+	System.out.println("val6 : " + var6);
+	System.out.println("val7 : " + var7);
+	System.out.println("val8 : " + var8);
+	System.out.println("val9 : " + var9);
+	
+	
+}
+}
